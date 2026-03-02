c6befd765f8844d79627d76ed83901b64d52b37b
Pim Pieters <pim@pieters.tv>
2026-03-02 20:36:08 +0100

    Rename DiceDistribution to Distribution

--- d20distribution/distribution.py
+++ d20distribution/distribution.py
@@ -14,7 +14,7 @@ def _combine_dictionaries(a: dict[int, float], b: dict[int, float], func: Callab
     return result
 
 
-class DiceDistribution(object):
+class Distribution(object):
     dist: dict[int, float]
 
     def __init__(self, values: dict[int, float]):
@@ -61,41 +61,41 @@ class DiceDistribution(object):
         ex_2 = self.mean() ** 2
         return math.sqrt(abs(e_x2 - ex_2))
 
-    def __add__(self, other: "DiceDistribution") -> "DiceDistribution":
-        return DiceDistribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a + b))
+    def __add__(self, other: "Distribution") -> "Distribution":
+        return Distribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a + b))
 
-    def __sub__(self, other: "DiceDistribution") -> "DiceDistribution":
-        return DiceDistribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a - b))
+    def __sub__(self, other: "Distribution") -> "Distribution":
+        return Distribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a - b))
 
-    def __mul__(self, other: "DiceDistribution") -> "DiceDistribution":
-        return DiceDistribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a * b))
+    def __mul__(self, other: "Distribution") -> "Distribution":
+        return Distribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a * b))
 
-    def __floordiv__(self, other: "DiceDistribution") -> "DiceDistribution":
-        return DiceDistribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a // b))
+    def __floordiv__(self, other: "Distribution") -> "Distribution":
+        return Distribution(_combine_dictionaries(self.dist, other.dist, lambda a, b: a // b))
 
-    def __neg__(self) -> "DiceDistribution":
-        return DiceDistribution({-v: k for v, k in self.dist.items()})
+    def __neg__(self) -> "Distribution":
+        return Distribution({-v: k for v, k in self.dist.items()})
 
-    def advantage(self, count: int = 2) -> "DiceDistribution":
+    def advantage(self, count: int = 2) -> "Distribution":
         if count < 1:
             raise InvalidOperationError(f"Rolling with advantage requires at least one roll, instead received {count}.")
 
         result = copy.copy(self.dist)
         for _ in range(1, count):
             result = _combine_dictionaries(result, self.dist, lambda a, b: max(a, b))
-        return DiceDistribution(result)
+        return Distribution(result)
 
-    def disadvantage(self, count: int = 2) -> "DiceDistribution":
+    def disadvantage(self, count: int = 2) -> "Distribution":
         if count < 1:
             raise InvalidOperationError(f"Rolling with disadvantage requires at least one roll, instead received {count}.")
 
         result = copy.copy(self.dist)
         for _ in range(1, count):
             result = _combine_dictionaries(result, self.dist, lambda a, b: min(a, b))
-        return DiceDistribution(result)
+        return Distribution(result)
 
-    def __copy__(self) -> "DiceDistribution":
-        return DiceDistribution(self.dist)
+    def __copy__(self) -> "Distribution":
+        return Distribution(self.dist)
 
-    def __deepcopy__(self) -> "DiceDistribution":
-        return DiceDistribution(copy.deepcopy(self.dist))
+    def __deepcopy__(self) -> "Distribution":
+        return Distribution(copy.deepcopy(self.dist))
