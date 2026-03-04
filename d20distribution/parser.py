2e3f97ef31c9a19377b250e340249e6090b1e590
Pim Pieters <pim@pieters.tv>
2026-03-04 18:06:15 +0100

    Add documentation to parser

--- d20distribution/parser.py
+++ d20distribution/parser.py
@@ -6,6 +6,19 @@ from .errors import DiceParseError
 
 
 def parse(expression: str) -> Distribution:
+    """Parse a valid d20 expression to a distribution. The performance of the expression
+    depends on the passed modifiers, as explained
+
+    Args:
+        expression (str): The dice expression, following the d20 style.
+
+    Raises:
+        DiceParseError: When an invalid expression is given.
+
+    Returns:
+        Distribution: A distribution built from the expression.
+    """
+
     # Attempt a roll first, to verify that the rolls are actually feasible
     try:
         d20.roll(expression)
@@ -15,10 +28,19 @@ def parse(expression: str) -> Distribution:
         raise DiceParseError("There was an error found while parsing the expression.")
 
     ast = d20.parse(expression, allow_comments=False)
-    return parse_ast(ast)
+    return _parse_ast(ast)
 
 
-def parse_dimensions(count: int, sides: str | int) -> tuple[int, int]:
+def _parse_dimensions(count: int, sides: str | int) -> tuple[int, int]:
+    """Parse the dimensions from a d20.ast.Dice or d20.ast.OperatedDice
+
+    Args:
+        count (int): The count of the dice.
+        sides (str | int): The sides of the dice.
+
+    Returns:
+        tuple[int, int]: The parsed values packed as a tuple, representing the count and sides respectively.
+    """
     if sides == "%":
         sides = 100
     else:
@@ -26,41 +48,53 @@ def parse_dimensions(count: int, sides: str | int) -> tuple[int, int]:
     return count, sides
 
 
-def parse_ast(ast: d20.ast.Node) -> Distribution:
+def _parse_ast(ast: d20.ast.Node) -> Distribution:
+    """Parse a distribution from a d20 ast node.
+
+    Args:
+        ast (d20.ast.Node): The node to be parsed.
+
+    Raises:
+        DiceParseError: When an unsupported node is parsed.
+
+    Returns:
+        Distribution: The distribution matching the node.
+    """
+
     if isinstance(ast, d20.ast.Expression):
-        return parse_ast(ast.roll)  # type: ignore
+        return _parse_ast(ast.roll)  # type: ignore
 
     if isinstance(ast, d20.ast.Literal):
         return Distribution({ast.value: 1.0})  # type: ignore
 
     if isinstance(ast, d20.ast.UnOp):
         if ast.op == "-":
-            return -parse_ast(ast.value)  # type: ignore
+            return -_parse_ast(ast.value)  # type: ignore
         if ast.op == "+":
-            return parse_ast(ast.value)  # type: ignore
+            return _parse_ast(ast.value)  # type: ignore
         raise DiceParseError(f"Unsupported UnOp operator '{ast.op}'.")
 
     if isinstance(ast, d20.ast.BinOp):
         if ast.op == "+":
-            return parse_ast(ast.left) + parse_ast(ast.right)  # type: ignore
+            return _parse_ast(ast.left) + _parse_ast(ast.right)  # type: ignore
         if ast.op == "-":
-            return parse_ast(ast.left) - parse_ast(ast.right)  # type: ignore
+            return _parse_ast(ast.left) - _parse_ast(ast.right)  # type: ignore
         if ast.op == "*":
-            return parse_ast(ast.left) * parse_ast(ast.right)  # type: ignore
+            return _parse_ast(ast.left) * _parse_ast(ast.right)  # type: ignore
         if ast.op == "/":
-            return parse_ast(ast.left) // parse_ast(ast.right)  # type: ignore
+            return _parse_ast(ast.left) // _parse_ast(ast.right)  # type: ignore
         raise DiceParseError(f"Unsupported BinOp operator '{ast.op}'.")
 
     if isinstance(ast, d20.ast.Parenthetical):
-        return parse_ast(ast.value)  # type: ignore
+        return _parse_ast(ast.value)  # type: ignore
 
     if isinstance(ast, d20.ast.Dice):
-        count, sides = parse_dimensions(ast.num, ast.size)
+        count, sides = _parse_dimensions(ast.num, ast.size)
         builder = ConvolutionDistributionBuilder(count, sides, [])
         return builder.distribution()
 
     if isinstance(ast, d20.ast.OperatedDice):
-        count, sides = parse_dimensions(ast.value.num, ast.value.size)  # type: ignore
+        count, sides = _parse_dimensions(ast.value.num, ast.value.size)  # type: ignore
         operations: list[d20.ast.SetOperator] = ast.operations  # type: ignore
 
         contains_non_convolution_operation = any(not ConvolutionDistributionBuilder.supports_operation(op) for op in operations)
